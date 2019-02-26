5e5489d2c585455a63989570b4672cdf619e10ed
Tomi <tomiris602@gmail.com>
2019-02-27 01:37:43 +0600

    test_vers

---
+++ Week-5/task1/task1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace task1
+{
+    public class Student
+    {
+
+        public int A { get; set; }
+        public int B { get; set; }
+        //public int I { get; } // = -1;
+        //public int X  { get; set; }
+       
+
+        public Student ()
+        {
+        }
+        
+        public void PrintInfo()
+        {
+            
+            if (B > 0)
+            {
+                Console.WriteLine(A+"+" + B+"i");
+            }
+            else
+            {
+                Console.WriteLine(A+""+B+"i");
+            }
+            Console.ReadLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F1();
+            F2();
+        }
+        private static void F1()
+        {
+            Student s = new Student();
+            s.A = int.Parse(Console.ReadLine()); 
+            s.B = int.Parse(Console.ReadLine());
+         
+
+
+
+            FileStream fs = new FileStream("student.xml", FileMode.Create, FileAccess.Write);
+            XmlSerializer xs = new XmlSerializer(typeof(Student));
+
+            xs.Serialize(fs, s);
+
+            fs.Close();
+
+        }
+        private static void F2()
+        {
+            FileStream fs = new FileStream("student.xml", FileMode.Open, FileAccess.Read);
+            XmlSerializer xs = new XmlSerializer(typeof(Student));
+            Student s = xs.Deserialize(fs) as Student;
+            s.PrintInfo();
+            fs.Close();
+        }
+    }
+}
