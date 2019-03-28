dd31125cc7daca429faea14b067b74aab3213041
Tomi <tomiris602@gmail.com>
2019-03-29 04:50:42 +0600

    new inf

---
+++ sample/SerializerFULL/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    public class Student
+    {
+        public int age;
+        public string name;
+        public Student()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void serialization(Student st)
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(Student));
+            FileStream fs = new FileStream("student.txt", FileMode.Create, FileAccess.Write);
+            xs.Serialize(fs, st);
+            fs.Close();
+        }
+        static Student deserialization()
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(Student));
+            FileStream fs = new FileStream("student.txt", FileMode.Open, FileAccess.Read);
+            return xs.Deserialize(fs) as Student;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Button F2 is Serialization");
+            Console.WriteLine("Button F3 is Deserialization");
+
+            Student st = new Student();
+            string n;
+            int a;
+            ConsoleKeyInfo pressedKey = Console.ReadKey();
+
+            switch (pressedKey.Key)
+            {
+                case ConsoleKey.F2:
+                    Console.Write("Enter Your name: ");
+                    n = Console.ReadLine();
+                    Console.Write("Enter Your age: ");
+                    a = int.Parse(Console.ReadLine());
+                    st.name = n;
+                    st.age = a;
+                    serialization(st);
+                    break;
+                case ConsoleKey.F3:
+                    st = deserialization();
+                    Console.WriteLine();
+                    Console.WriteLine("Your Name Is " + st.name);
+                    Console.WriteLine("Your Age Is " + st.age);
+                    break;
+            }
+        }
+    }
+}
---
+++ sample/threadSample'S/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread th = new Thread(func);
+            Thread th2 = new Thread(punc);
+            th.Start();
+            th2.Start();
+        }
+
+        private static void func()
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                Console.Write(i + " ");
+                Thread.Sleep(4);
+            }
+        }
+
+        private static void punc()
+        {
+            for (int i = 6; i <= 10; i++)
+            {
+                Console.Write(i + " ");
+                Thread.Sleep(3);
+            }
+
+        }
+    }
+}
