79fc93ebca9d1209ad852f1720c06323c2a30e2a
Tomi <tomiris602@gmail.com>
2019-03-29 14:12:02 +0600

    new tack with time

---
+++ sample/Time/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ИДЗ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread date = new Thread(Time);
+            Thread substr = new Thread(String);
+            date.Start();
+            Thread.Sleep(100);
+            substr.Start();
+        }
+        public static void Time()
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("End.");
+        }
+        public static void String()
+        {
+            string s = "University";
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.WriteLine(s.Substring(i));
+            }
+            Console.WriteLine("End.");
+        }
+
+
+    }
+}
---
+++ sample/Time2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Threading;
+
+namespace Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Timers.Timer timer = new System.Timers.Timer(100);
+            timer.Elapsed += Elapse;
+
+            timer.Enabled = true;
+            Thread.CurrentThread.Join();
+        }
+
+        private static void Elapse(object sender, ElapsedEventArgs e)
+        {
+            string s = Console.ReadLine();
+            DateTime time = DateTime.Now;
+            Console.WriteLine(s[0] + " " + s[1] + " " + s[2] + " " + s[3] + " " + s[4] + " " + s[5]);
+
+            Console.WriteLine(time.DayOfWeek + ": " + time.Hour + ":" + time.Minute + ":" + time.Second + ":" + time.Millisecond);
+        }
+    }
+}
